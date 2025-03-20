5e3131715580b56d75535f9874c1aaf428e58f9a
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2025-03-20 23:29:15 +0000

    change min

--- src/models/character.ts
+++ src/models/character.ts
@@ -1,7 +1,8 @@
-import { minLength, object, pipe, string } from "valibot";
+import { minLength, object, pipe, string, type InferInput } from "valibot";
 
 export const CharacterSchema = object({
     name: pipe(string(), minLength(2)),
     lastName: pipe(string(), minLength(4))
+});
 
-})
+export type Character = InferInput<typeof CharacterSchema> & {id: number}
